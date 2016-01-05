5c53cf9b62f1166d88d9c738d27ee67e75070cea
MEGApixel23 <megapixel23@ya.ru>
2016-01-05 12:29:47 +0200

    Initial

---
+++ echo.exs
@@ -0,0 +1,19 @@
+defmodule Recursion do
+	def run(func) do
+		IO.puts(inspect func.())
+		run(func)
+	end
+end
+
+Recursion.run(fn ->	
+  input = IO.gets "> "
+  compare_input = String.replace(String.downcase(input), "\n", "")
+  compare_input = String.replace(compare_input, " ", "")
+
+	if compare_input == "quit" do
+    {:ok, 1}
+  else
+  	IO.puts "Echo: " <> input
+    {:error, 0}
+  end
+end)
