1e475edb164b9f0fb620b674b3c9ce26a8dee9df
MEGApixel23 <megapixel23@ya.ru>
2016-01-05 12:34:14 +0200

    tab to spaces conversion

--- echo.exs
+++ echo.exs
@@ -1,19 +1,20 @@
 defmodule Recursion do
-	def run(func) do
-		IO.puts(inspect func.())
-		run(func)
-	end
+  def run(func) do
+    IO.puts(inspect func.())
+    run(func)
+  end
 end
 
-Recursion.run(fn ->	
+Recursion.run(fn -> 
   input = IO.gets "> "
+  
   compare_input = String.replace(String.downcase(input), "\n", "")
   compare_input = String.replace(compare_input, " ", "")
 
-	if compare_input == "quit" do
+  if compare_input == "quit" do
     {:ok, 1}
   else
-  	IO.puts "Echo: " <> input
+    IO.puts "Echo: " <> input
     {:error, 0}
   end
 end)
